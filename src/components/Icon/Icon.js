e3bc3365839a6411b04b375cdb8273b9a9600092
lavegaa <cksgh405@naver.com>
2020-10-15 23:50:36 +0900

    feat: 아이콘 컴포넌트 마무리 (#9)

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -2,30 +2,109 @@ import React from "react";
 import classNames from "classnames/bind";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import iconImage from "./../../assets/images/sample.png";
+import iconImage from "./../../assets/images/icons.png";
 
 const I = styled.i`
   display: inline-block;
   background-image: url(${iconImage});
-  background-size: 291px 189px;
-  width: ${props => props.size === "xsm" ? "24px" :
-                    props.size === "sm" ? "27px" :
-                    props.size === "md" ? "36px" :
-                    props.size === "lmd" ? "40px" :
-                    props.size === "lg" ? "58px" : "24px" };  
-  height: ${props => props.size === "xsm" ? "24px" :
-                     props.size === "sm" ? "27px" :
-                     props.size === "md" ? "36px" :
-                     props.size === "lmd" ? "40px" :
-                     props.size === "lg" ? "58px" : "24px" };  
-  background-position: ${props => props.type === "test1" ? "0 0" :
-                                  props.type === "test2" ? "-38px 0" : "-76px 0"};
+  background-size: 928px 708px;
+  background-color: green;
+  width: ${props => props.size === "xsm" ? "34px" :
+                    props.size === "sm" ? "38px" :
+                    props.size === "md" ? "60px" :
+                    props.size === "lg" ? "70px" : "900px" };  
+  height: ${props => props.size === "xsm" ? "34px" :
+                     props.size === "sm" ? "38px" :
+                     props.size === "md" ? "60px" :
+                     props.size === "lg" ? "70px" : "700px" };  
+  background-position: ${props => props.type === "bubble_white" ? "-75.5px -66px" :
+                                  props.type === "bubble_black" ? "-158.5px -66px" :
+                                  props.type === "arrow_down" ? "-241.5px -66px" :
+                                  props.type === "arrow_up" ? "-323px -66px" :
+                                  props.type === "post" ? "-403.5px -66px" :
+                                  props.type === "cancel_blue" ? "-486.5px -66px" :
+                                  props.type === "cancel_black" ? "-569.5px -66px" :
+                                  props.type === "check" ? "-653.5px -66px" :
+                                  props.type === "search" ? "-733.5px -66px" :
+                                  props.type === "profile_white" ? "-75.5px -170px" :
+                                  props.type === "profile_black" ? "-158.5px -170px" :
+                                  props.type === "circle" ? "-240px -171px" :
+                                  props.type === "drawer" ? "-320.5px -169.5px" :
+                                  props.type === "check_off" ? "-402.5px -169.5px" :
+                                  props.type === "check_on" ? "-485px -169px" :
+                                  props.type === "sound_white" ? "-75.5px -276px" :
+                                  props.type === "sound_black" ? "-158.5px -276px" :
+                                  props.type === "all_off" ? "-227px -265px" :
+                                  props.type === "sun_off" ? "-310px -265px" :
+                                  props.type === "mon_off" ? "-391px -265px" :
+                                  props.type === "tue_off" ? "-474px -265px" :
+                                  props.type === "wed_off" ? "-557px -265px" :
+                                  props.type === "thu_off" ? "-638px -265px" :
+                                  props.type === "fri_off" ? "-720px -265px" :
+                                  props.type === "sat_off" ? "-803px -265px" :
+                                  props.type === "exit_white" ? "-75.5px -380px" :
+                                  props.type === "exit_black" ? "-158.5px -380px" :
+                                  props.type === "all_on" ? "-227px -370px" :
+                                  props.type === "sun_on" ? "-310px -370px" :
+                                  props.type === "mon_on" ? "-391px -370px" :
+                                  props.type === "tue_on" ? "-474px -370px" :
+                                  props.type === "wed_on" ? "-557px -370px" :
+                                  props.type === "thu_on" ? "-638px -370px" :
+                                  props.type === "fri_on" ? "-720px -370px" :
+                                  props.type === "sat_on" ? "-803px -370px" :
+                                  props.type === "time_white" ? "-76px -488px" :
+                                  props.type === "time_black" ? "-158px -488px" :
+                                  props.type === "previous" ? "-227px -475px" :
+                                  props.type === "next" ? "-309px -475px" :
+                                  props.type === "minus" ? "-391px -475px" :
+                                  props.type === "plus" ? "-473px -475px" :
+                                  props.type === "add_white" ? "-73px -594px" :
+                                  props.type === "add_black" ? "-158px -594px" :
+                                  props.type === "check_circle" ? "-228px -582px" :
+                                  props.type === "play" ? "-309px -580px" :
+                                  props.type === "cancel_circle" ? "-391px -580px" :
+                                  props.type === "check_border" ? "-474px -580px" :
+                                  props.type === "check_Square" ? "-550px -576px" : "0 0"};
 
 `;
 const cx = classNames.bind();
 
 export default function Icon(props) {
-  const { type, size, alt, style} = props;
+  const { type, alt, style} = props;
+  let size = "xsm"
+  if(type === "drawer" || type === "check_off" || type === "check_on") {
+    size = "sm"
+  }
+  if(type === "all_off" || 
+     type === "sun_off" || 
+     type === "mon_off" || 
+     type === "tue_off" || 
+     type === "wed_off" || 
+     type === "thu_off" || 
+     type === "fri_off" || 
+     type === "sat_off" ||
+     type === "all_on" || 
+     type === "sun_on" || 
+     type === "mon_on" || 
+     type === "tue_on" || 
+     type === "wed_on" || 
+     type === "thu_on" || 
+     type === "fri_on" || 
+     type === "sat_on" ||
+     type === "next" ||
+     type === "minus" ||
+     type === "plus" ||
+     type === "previous" ||
+     type === "check_circle" ||
+     type === "play" ||
+     type === "cancel_circle" ||
+     type === "check_border" )  {
+       size = "md"
+     }
+  if(type === "check_Square") {
+    size = "lg"
+  }
+
   if (props.src) {
     const { src } = props;
     return (
@@ -47,8 +126,54 @@ export default function Icon(props) {
 }
 Icon.propTypes = {
   type: PropTypes.oneOf([
-    "test1",
-    "test2"
+    "bubble_white" ,
+    "bubble_black", 
+    "arrow_down", 
+    "arrow_up",
+    "post",
+    "cancel_blue",
+    "cancel_black",
+    "check",
+    "search",
+    "profile_white",
+    "profile_black",
+    "circle",
+    "drawer",
+    "check_off",
+    "check_on",
+    "sound_white",
+    "sound_black",
+    "all_off",
+    "sun_off",
+    "mon_off",
+    "tue_off",
+    "wed_off",
+    "thu_off",
+    "fri_off",
+    "sat_off",
+    "exit_white", 
+    "exit_black", 
+    "all_on",
+    "sun_on",
+    "mon_on",
+    "tue_on",
+    "wed_on",
+    "thu_on",
+    "fri_on",
+    "sat_on", 
+    "time_white",
+    "time_black", 
+    "previous",
+    "next",
+    "minus",
+    "plus",
+    "add_white",
+    "add_black",
+    "check_circle", 
+    "play",
+    "cancel_circle",
+    "check_border", 
+    "check_Square", 
   ]),
   size: PropTypes.oneOf(["xsm", "sm", "md", "lmd", "lg"]),
   alt: PropTypes.string,
@@ -61,5 +186,3 @@ Icon.defaultProp = {
   size: "xms",
   alt: ""
 };
-
-
