766e98ad946523b16201bc69473a21775bead93f
lavegaa <cksgh405@naver.com>
2020-10-19 00:28:19 +0900

    feat: Icon component에 대한 story 작성 (#9)

---
+++ src/components/Icon/Icon.stories.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+
+import Icon from './Icon';
+
+export default {
+  title: 'icon',
+  component: Icon,
+};
+const Template = (args) => <Icon {...args} />;
+
+export const icon = Template.bind({});
