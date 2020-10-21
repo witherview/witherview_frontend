f7d7c62e8efedc89da8347ed82bcd289373d4c95
Ki Tae Park <rlxo1651@daum.net>
2020-10-21 15:03:40 +0900

    feat(inputBar 생성): 텍스트 입력받는 input 창 생성

--- src/components/InputBar/InputBar.js
+++ src/components/InputBar/InputBar.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const input = styled.input`
-    width: 533px;
-    border: none;
-    border-bottom: 2px solid #9e9e9e;
-    font-size: 20px;
-    line-height: 1.3;
-    text-align: left;
-    color: #9e9e9e;
-`
+const Wrapper = styled.input`
+  width: 533px;
+  border: none;
+  border-bottom: 2px solid #9e9e9e;
+  font-size: 20px;
+  font-family: AppleSDGothicNeoM00;
+  line-height: 1.7;
+  text-align: left;
+  color: #9e9e9e;
+  &:focus {
+    outline: none;
+    border-bottom: 3px solid #5f5fd9;
+    color: #3d3d3d;
+  }
+`;
 
-export default function InputBar({
-    text
-}) {
-    return <input type="text" placeholder="기업명을 입력해주세요" />
+export default function InputBar() {
+  return (
+    <Wrapper type="text" placeholder="기업명을 입력해주세요" />
+  );
 }
-
-Icon.defaultProp = {
-    type: 'test',
-    alt: '',
-  };
-  
