b6b87fcb0828b848b996e554b3f0874d91c50a60
lavegaa <cksgh405@naver.com>
2020-10-22 09:51:56 +0900

    feat: Button컴포넌트 삭제 (#8)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Button from './components/Button';
 import NotFound from './pages/404';
 
 export default function App() {
   return (
     <>
       <Switch>
-        <Route exact path="/" component={Button} />
+        <Route exact path="/" component={NotFound} />
         <Route component={NotFound} />
       </Switch>
     </>
--- src/components/Button.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import TimeButton from './TimeButton';
-
-export default function Button() {
-  return <TimeButton time={45} />;
-}
