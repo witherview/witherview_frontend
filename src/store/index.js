4ed28ddd36d1a81f63468c3a3f838967124ba612
lavegaa <cksgh405@naver.com>
2020-10-23 09:09:23 +0900

    feat: 코드리뷰 반영 (#8)

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-// import reducer from './Slice/slice';
 import timeReducer from './Time/time';
 
 export const reducers = combineReducers({
