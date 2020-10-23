bf9cfba8c617bd6f9fcc4974ca35a5ba16ffca8b
Ki Tae Park <rlxo1651@daum.net>
2020-10-23 20:19:21 +0900

    feat(Checkbox): Checkbox 컴포넌트 작성

---
+++ src/components/Checkbox/Checkbox.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Checkbox from './Checkbox';
+
+export default {
+  title: 'checkbox',
+  component: Checkbox,
+};
+
+export const CheckBox = (args) => <Checkbox {...args} />;
