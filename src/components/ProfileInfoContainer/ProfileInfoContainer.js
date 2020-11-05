21b8b5aebad6e4ae4f30a098e9e9b57341333697
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-11-06 02:01:22 +0900

    feat: 상대방 프로필(이름, 참여도) 컴포넌트 컨테이너 완성 (#33) - [ ] 숫자 폰트 설정 필요

---
+++ src/components/ProfileInfoContainer/ProfileInfoContainer.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+import styled from 'styled-components';
+
+import ProfileIcon from '../ProfileIcon';
+
+import profileExample from '../../assets/images/profile_example.png';
+
+const Wrapper = styled.div`
+  height: 73px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const Name = styled.div`
+  font-family: AppleSDGothicNeoEB00;
+  font-size: 20px;
+  padding-left: 25px;
+  user-select: none;
+  color: #3d3d3d;
+`;
+
+const NameBelow = styled.div`
+  font-family: AppleSDGothicNeoEM00;
+`;
+
+export default function ProfileInfoContainer({ name, participateRate, src }) {
+  return (
+    <Wrapper>
+      <ProfileIcon src={src} size="small" />
+      <Name>
+        <div>{name}</div>
+        <NameBelow>
+          {participateRate}
+          %의 참여도
+        </NameBelow>
+      </Name>
+    </Wrapper>
+  );
+}
+
+ProfileInfoContainer.propTypes = {
+  name: PropTypes.string,
+  participateRate: PropTypes.number,
+  src: PropTypes.string,
+};
+
+ProfileInfoContainer.defaultProps = {
+  name: '홍길동',
+  participateRate: 99,
+  src: profileExample,
+};
