aacf12139d96537783b171b8f71867f63b5def3e
Ki Tae Park <rlxo1651@daum.net>
2020-11-11 21:04:44 +0900

    feat: 혼자연습하기 체크리스트 가이드 화면 제작- 기본화면 구현(#42)

---
+++ src/pages/AloneQuestionCheckList/AloneQuestionCheckList.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import AloneQuestionCheckList from "./AloneQuestionCheckList";
+
+export default {
+    title: 'AloneQuestionCheckList',
+    component: AloneQuestionCheckList
+}
+
+export const CheckList = (args) => <AloneQuestionCheckList {...args} />
