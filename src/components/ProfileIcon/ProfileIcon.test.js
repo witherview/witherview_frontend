c04280ff341961de2ac2c3b9837f8dd0b4dc2fb6
dididy <dididy@dididys-MBP.Davolink>
2020-11-12 19:59:43 +0900

    fix: Jest에서 png 잘못 읽는 문제 해결 (#40) - https://github.com/facebook/jest/issues/2663#issuecomment-362261679 - 테스트코드 일부 추가

--- src/components/ProfileIcon/ProfileIcon.test.js
+++ src/components/ProfileIcon/ProfileIcon.test.js
@@ -4,7 +4,9 @@ import { render } from '@testing-library/react';
 
 import ProfileIcon from './index';
 
-function renderProfileIcon(src, size) {
+const profileImage = 'https://avatars2.githubusercontent.com/u/16266103?s=460&u=46ab2774d38212f0d0050592ce02dbcf36a7a97a&v=4';
+
+function renderProfileIcon(src=profileImage, size) {
   return render(<ProfileIcon src={src} size={size} />);
 }
 
