17735f45a4642f255235c9ac8a25f4e9a4e6ac14
dididy <dev.yongjaelee@gmail.com>
2020-11-20 11:06:11 +0900

    refactor(lint): lint 에러 발생하는 부분 수정 - ProfileInfoContainer.test.js에 띄어쓰기 제거

--- src/components/ProfileInfoContainer/ProfileInfoContainer.test.js
+++ src/components/ProfileInfoContainer/ProfileInfoContainer.test.js
@@ -6,7 +6,7 @@ import ProfileInfoContainer from './index';
 
 const profileImage = 'https://avatars2.githubusercontent.com/u/16266103?s=460&u=46ab2774d38212f0d0050592ce02dbcf36a7a97a&v=4';
 
-function renderProfileInfoContainer(name='홍길동', participateRate=90, src=profileImage) {
+function renderProfileInfoContainer(name = '홍길동', participateRate = 90, src = profileImage) {
   return render(<ProfileInfoContainer name={name} pariticpateRate={participateRate} src={src} />);
 }
 
