ac3586ddb378e87895da6fdcad2464f210c41cc2
Ki Tae Park <rlxo1651@daum.net>
2020-11-21 21:44:14 +0900

    refactor(리팩토링): 불필요한 코드 제거 - #43

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Route, Switch } from 'react-router-dom';
 import ConferenceButton from './components/ConferenceButton';
 import ConferenceRoom from './pages/ConferenceRoom';
 import NotFound from './pages/404';
-import ModalWrapper from './pages/ModalWrapper';
 
 export default function App() {
   return (
@@ -14,7 +13,6 @@ export default function App() {
         <Route path="/room/:roomID" component={ConferenceRoom} />
         <Route component={NotFound} />
       </Switch>
-      <ModalWrapper></ModalWrapper>
     </>
   );
 }
