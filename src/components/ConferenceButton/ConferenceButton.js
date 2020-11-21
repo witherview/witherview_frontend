d64df8d401c543847a65ab801484decc1bcd5077
Ki Tae Park <rlxo1651@daum.net>
2020-11-21 21:29:53 +0900

    feat(모달 제작): 메인화면에서 버튼 클릭하면 모달 보여지도록 함 - #43

--- src/components/ConferenceButton/ConferenceButton.js
+++ src/components/ConferenceButton/ConferenceButton.js
@@ -2,15 +2,25 @@ import React from 'react';
 import ReactRouterPropTypes from 'react-router-prop-types';
 
 import { v1 as uuid } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { showModal, hideModal } from '../../store/Modal/modal';
+import Modal from '../Modal/Modal';
+import { MODALS } from '../../utils/constant';
 
 export default function ConferenceButton({ history }) {
+  const dispatch = useDispatch();
   const handleCreate = () => {
     const id = uuid();
     history.push(`/room/${id}`);
   };
 
   return (
-    <button type="button" onClick={() => handleCreate()}>컨퍼런스 생성</button>
+    <>
+      <Modal modalName={MODALS.QUESTIONLIST_SAVE_MODAL} />
+      <button type="button" onClick={() => handleCreate()}>컨퍼런스 생성</button>
+      <button onClick={() => dispatch(showModal(MODALS.QUESTIONLIST_SAVE_MODAL))} type="button">Modal open</button>
+      <button onClick={() => dispatch(hideModal(MODALS.QUESTIONLIST_SAVE_MODAL))} type="button">Modal close</button>
+    </>
   );
 }
 
