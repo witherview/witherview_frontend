84450cf02190a96a112e3f9ba27f9588b26f0009
dididy <dev.yongjaelee@gmail.com>
2020-11-27 22:36:49 +0900

    feat: 혼자 연습하기 로딩 및 연습 페이지 퍼블리싱 완성 (#70)
    - [ ] 질문 리스트 API 연동
    - [ ] 앞페이지에서 시간, 이름, 직무 상태 들고오도록 수정
    - [ ] 다음페이지를 위해 구간 저장 로직 추가
    - [ ] 다음 페이지로 넘어가는 로직 추가(history.push 쓸 듯)
    
    - Fixture, Style 분리
    - 세부 페이지 트렌지션 구현 완료

--- src/pages/SelfTrainPage/SelfTrainPage.js
+++ src/pages/SelfTrainPage/SelfTrainPage.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
+import React, {
+  useState, useEffect, useCallback, useRef,
+} from 'react';
 
 import Sidebar from '../../components/Sidebar';
 import ProfileMenuContainer from '../../components/ProfileMenuContainer';
@@ -9,132 +10,203 @@ import TextBox from '../../components/TextBox';
 import Button from '../../components/Button';
 import Icon from '../../components/Icon';
 
+import RemainTime from '../../components/RemainTime';
+import ToggleButton from '../../components/ToggleButton';
+import AnswerBox from '../../components/AnswerBox';
+import QuestionTextBox from '../../components/QuestionTextBox';
 import useWindowSize from '../../hooks/useWindowSize';
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-const WrapContainer = styled.div`
-  height: ${({ height }) => height}px;
-  width: 100%;
-`;
-
-const WrapTop = styled.div`
-  @media only screen and (max-width: 480px) {
-    display: none;
-  }
-
-  position: absolute;
-  top: 53px;
-  right: 105px;
-`;
-
-const WrapMiddle = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const WrapCamView = styled.div`
-  padding: 40px;
-`;
+import Fixture from './transitionFixture';
+import S from './SelfTrainPage.style';
 
 const STEP_FIRST = 0;
 const STEP_LOADING_1 = 1;
 const STEP_LOADING_2 = 2;
 const STEP_START = 3;
 const STEP_ING = 4;
+const TOGGLE_SCRIPT = 5;
 
-const name = '홍길동';
-const company = '카카오 서비스 기획';
-const Fixture = [
+// TODO: 앞 페이지 완성하면 거기서 상태를 들고오도록 수정
+const TIME = 45;
+const NAME = '홍길동';
+const COMPANY = '카카오 서비스 기획';
+
+const QNA_LIST = [
   {
-    top: '기본 질문을 시작하겠습니다.',
-    bottom: '먼저 마이크와 카메라를 준비해주세요.',
-    button: '시작하기',
+    id: 6,
+    question: '질문입니다.',
+    answer: '답변입니다.',
+    order: 0,
   },
   {
-    top: '실제 면접을 본다고 생각하고 상황을 상상해보세요',
-    bottom: '똑바로 앉으시고, 카메라나 화면을 응시해 주세요.',
-  },
-  {
-    top: '면접 장소에 도착했습니다.',
-    bottom: '똑바로 앉으시고, 카메라나 화면을 응시해 주세요.',
-  },
-  {
-    top: '3분 뒤 면접을 시작하겠습니다.',
-    bottom: '3분 동안 예상 면접 질문에 대해 답변을 생각해 보세요.',
-    button: '바로 시작하기',
+    id: 8,
+    question: 'string',
+    answer: 'string',
+    order: 1,
   },
 ];
 
 export default function SelfTrainPage() {
   const { height, width } = useWindowSize();
-  const [step, useStep] = useState(STEP_FIRST);
+  const resetId = useRef();
 
-  const [timer, useTimer] = useState(null);
+  const [step, setStep] = useState(STEP_FIRST);
+  const [time, setTime] = useState(180);
 
-  const handleNext = () => {
-    if (step === STEP_FIRST) {
-      useStep(STEP_LOADING_1);
-    }
-    if (step === STEP_START) {
-      useStep(STEP_ING);
-    }
+  const [qnaStep, setQnaStep] = useState(0);
+
+  const init = () => {
+    clearTimeout(resetId.current);
+    clearInterval(resetId.current);
   };
 
   const handleCancel = () => {
-    clearTimeout(timer);
-    useStep(STEP_FIRST);
-    useTimer(null);
+    setStep(STEP_FIRST);
+    setTime(0);
+    setQnaStep(0);
+    init();
   };
 
-  useEffect(() => {
-    if (step === STEP_LOADING_1 || step === STEP_LOADING_2) {
-      const setTimer = setTimeout(() => {
-        useStep(step + STEP_LOADING_1);
-      }, 3000);
-      useTimer(setTimer);
+  const handleCountTimer = useCallback((initTime) => {
+    setTime(initTime);
+    let i = 0;
+    resetId.current = setInterval(() => {
+      setTime(initTime - i);
+      i += 1;
+    }, 1000);
+  }, [resetId, time]);
+
+  const handleStepQuestion = () => {
+    init();
+    handleCountTimer(TIME);
+    if (qnaStep === QNA_LIST.length) {
+      // TODO: 다음 페이지로 넘어가는 로직 추가해야 함
+      return () => {};
+    }
+    return setQnaStep(qnaStep + 1);
+  };
+
+  const handleNext = useCallback(() => {
+    if (step === STEP_FIRST) {
+      setStep(STEP_LOADING_1);
+    }
+    if (step === STEP_START) {
+      setStep(STEP_ING);
+      init();
+      handleCountTimer(TIME);
+    }
+    if (step === STEP_ING || step === TOGGLE_SCRIPT) {
+      handleStepQuestion();
     }
   }, [step]);
 
+  useEffect(() => {
+    if (step === STEP_LOADING_1 || step === STEP_LOADING_2) {
+      resetId.current = setTimeout(() => {
+        setStep(step + 1);
+      }, 3000);
+    }
+
+    if (step === STEP_START) {
+      handleCountTimer(180);
+    }
+  }, [step]);
+
+  useEffect(() => {
+    if (time === 0) {
+      init();
+      if (step === STEP_START) {
+        setStep(STEP_ING);
+        setTime(60);
+      }
+
+      if (step === STEP_ING || step === TOGGLE_SCRIPT) {
+        handleStepQuestion();
+      }
+    }
+  }, [time]);
+
   return (
-    <Wrapper>
+    <S.Wrapper>
       {step === STEP_FIRST ? <Sidebar /> : <></>}
-      <WrapContainer height={height}>
-        <WrapTop>
+      <S.WrapContainer height={height}>
+        <S.WrapAbsolute>
           {step === STEP_FIRST ? (
             <ProfileMenuContainer />
           ) : (
-            <Icon type="cancel_circle" func={handleCancel} alt="cancel" />
+            <Icon
+              isCircle
+              type="cancel_circle"
+              func={handleCancel}
+              alt="cancel"
+            />
           )}
-        </WrapTop>
-        <WrapMiddle>
-          <TextBox
-            topText={
-              (step === STEP_LOADING_2 ? `${name}님은 ${company} ` : '')
-              + Fixture[step].top
-            }
-            bottomText={Fixture[step].bottom}
-          />
-          <WrapCamView>
-            <CamView height={width / 3} width={width / 1.5} />
-          </WrapCamView>
-          {Fixture[step].button ? (
-            <Button
-              theme="blue"
-              text={Fixture[step].button}
-              func={handleNext}
+        </S.WrapAbsolute>
+        <S.WrapContent>
+          {step <= STEP_START ? (
+            <TextBox
+              topText={
+                (step === STEP_LOADING_2 ? `${NAME}님은 ${COMPANY} ` : '')
+                + Fixture[step].top
+              }
+              bottomText={Fixture[step].bottom}
             />
           ) : (
-            <></>
+            <QuestionTextBox
+              question={QNA_LIST[qnaStep].question}
+              order={QNA_LIST[qnaStep].order + 1}
+              width={width / 1.5}
+            />
           )}
-        </WrapMiddle>
-      </WrapContainer>
-    </Wrapper>
+
+          <S.WrapCamView width={width / 1.5}>
+            <CamView
+              height={width / 3}
+              width={step === TOGGLE_SCRIPT ? width / 1.5 - 553 : width / 1.5}
+            />
+            {/* TODO: Server API(DATE) 반영되면 적용 */}
+            {step === TOGGLE_SCRIPT ? (
+              <AnswerBox answer={QNA_LIST[qnaStep].answer} />
+            ) : (
+              <></>
+            )}
+          </S.WrapCamView>
+          <S.WrapBottom width={width / 1.5}>
+            <S.WrapBottomSide>
+              {step === STEP_START
+              || step === STEP_ING
+              || step === TOGGLE_SCRIPT ? (
+                <RemainTime time={time} />
+                ) : (
+                  <></>
+                )}
+            </S.WrapBottomSide>
+            {/* STEP_ING의 경우 버튼 누를 때 구간 저장하는 로직 추가 필요 */}
+            {Fixture[step].button ? (
+              <Button
+                theme="blue"
+                text={Fixture[step].button}
+                func={handleNext}
+              />
+            ) : (
+              <></>
+            )}
+            <S.WrapBottomSide right>
+              {step === STEP_ING || step === TOGGLE_SCRIPT ? (
+                <>
+                  <S.WrapText>답변 보기 허용</S.WrapText>
+                  <ToggleButton
+                    funcActive={() => setStep(TOGGLE_SCRIPT)}
+                    funcDeactive={() => setStep(STEP_ING)}
+                  />
+                </>
+              ) : (
+                <></>
+              )}
+            </S.WrapBottomSide>
+          </S.WrapBottom>
+        </S.WrapContent>
+      </S.WrapContainer>
+    </S.Wrapper>
   );
 }
