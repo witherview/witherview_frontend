f0ba2d8a148a596faad717c019fe539ded4c49ea
dididy <dev.yongjaelee@gmail.com>
2020-11-29 21:49:47 +0900

    feat: ProfileMenuContainer에 메뉴 아이템 및 토글, 버튼 동작 완성 (#75)
    - 로그아웃 동작하도록 추가
    - 퍼블리싱

--- src/components/ProfileMenuContainer/ProfileMenuContainer.js
+++ src/components/ProfileMenuContainer/ProfileMenuContainer.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import PropTypes from 'prop-types';
 
 import styled from 'styled-components';
 
+import { useDispatch } from 'react-redux';
+import { setLogout } from '../../store/Auth/auth';
+
 import Icon from '../Icon';
 import ProfileIcon from '../ProfileIcon';
-
 import profileExample from '../../assets/images/profile_example.png';
 
 const Wrapper = styled.div`
+  @media only screen and (max-width: 480px) {
+    display: none;
+  }
+
+  position: absolute;
+  top: 53px;
+  right: 105px;
   height: 73px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const WrapMenu = styled.div`
+  list-style-type: none;
+  position: relative;
+  text-align: center;
+  z-index: 101;
+  fill: ${({ isOpen }) => isOpen && '#0b3895'};
+  transform: ${({ isOpen }) => isOpen && 'scale(1)'};
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -25,12 +46,75 @@ const Name = styled.div`
   color: #3d3d3d;
 `;
 
+const List = styled.ul`
+  width: 143px;
+  padding-top: 18.5px;
+  padding-bottom: 18.5px;
+  position: absolute;
+  top: 70px;
+  right: 0px;
+  z-index: 101;
+  background-color: #fff;
+  transition: 0.25s ease all;
+  transform: scale(0);
+  transform-origin: 0 1;
+  border-radius: 10px;
+  box-shadow: 0 12px 24px 0 rgba(4, 4, 161, 0.15);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  transform: ${({ isOpen }) => isOpen && 'scale(1)'};
+`;
+
+const Item = styled.li`
+  width: 87px;
+  padding-top: 12.5px;
+  padding-bottom: 12.5px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+const Each = styled.div`
+  user-select: none;
+  font-family: AppleSDGothicNeoM00;
+  font-size: 20px;
+  color: #9e9e9e;
+  &:hover {
+    color: #f2886b;
+    text-decoration: none;
+  }
+`;
+
 export default function ProfileMenuContainer({ name, src }) {
+  const dispatch = useDispatch();
+
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = (set) => setIsOpen(set);
+
   return (
     <Wrapper>
-      <ProfileIcon src={src} />
-      <Name>{name}</Name>
-      <Icon type="arrow_down_grey" alt="arrow_down_grey" />
+      <WrapMenu
+        isOpen={isOpen}
+        onMouseOver={() => toggle(true)}
+        onMouseLeave={() => toggle(false)}
+      >
+        <ProfileIcon src={src} />
+        <Name>{name || 'Unknown'}</Name>
+
+        <Icon type="arrow_down_grey" alt="arrow_down_grey" />
+        <List isOpen={isOpen}>
+          <Item>
+            {/* TODO: history.push('mypage') */}
+            <Each onClick={() => {}}>마이페이지</Each>
+          </Item>
+          <Item>
+            <Each onClick={() => dispatch(setLogout())}>로그아웃</Each>
+          </Item>
+        </List>
+      </WrapMenu>
     </Wrapper>
   );
 }
