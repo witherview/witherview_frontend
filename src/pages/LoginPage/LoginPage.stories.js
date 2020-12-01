1faac1616465152cae2b86e4c0b186a7c3694260
dididy <dev.yongjaelee@gmail.com>
2020-12-02 00:37:14 +0900

    style: LoginPage 와이어프레임 적용 퍼블리싱 완성 (#82)
    - storybook 연결
    - 위더뷰 다크 로고 추가
    - 로그인 테스트 성공

---
+++ src/pages/LoginPage/LoginPage.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import LoginPage from './LoginPage';
+
+export default {
+  title: 'page/LoginPage',
+  component: LoginPage,
+};
+
+export const LoginPageDefault = () => <LoginPage />;
