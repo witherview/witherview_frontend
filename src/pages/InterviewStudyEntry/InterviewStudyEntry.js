5458f23ad9d0c5c91318e20f104e416d84a024c4
Ki Tae Park <rlxo1651@daum.net>
2020-12-05 19:42:11 +0900

    feat(면접스터디): 스터디방 정보 부분 구현 - #102

--- src/pages/InterviewStudyEntry/InterviewStudyEntry.js
+++ src/pages/InterviewStudyEntry/InterviewStudyEntry.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import styled from 'styled-components';
+import Button from '@components/Button/Button';
 
 const Wrapper = styled.div`
   flex: 1;
@@ -18,7 +19,9 @@ const InterviewRoomSection = styled.div`
 `;
 
 const InterviewRoomInfo = styled.div`
+  display: flex;
   width: 100%;
+  justify-content: space-between;
   padding: 50px;
   border-radius: 20px;
   box-shadow: 0 6px 12px 0 rgba(4, 4, 161, 0.1);
@@ -30,12 +33,52 @@ const ChatWithUser = styled.div`
   width: 474px;
 `;
 
+const TextWrapper = styled.div`
+  width: 750px;
+`;
+
+const RoomTitle = styled.p`
+  font-size: 36px;
+  font-family: AppleSDGothicNeoEB00;
+  color: #6e6eff;
+  margin-bottom: 15px;
+`;
+
+const DateInfo = styled.p`
+  font-size: 24px;
+  font-family: AppleSDGothicNeoEB00;
+  margin-bottom: 45px;
+`;
+
+const Description = styled.p`
+  font-size: 24px;
+  font-family: AppleSDGothicNeoM00;
+  color: #3d3d3d;
+`;
+
+const BoxWrapper = styled.div`
+  width: 300px;
+  & div:first-child {
+    margin-bottom: 25px;
+  }
+`;
+
 export default function InterviewStudyEntry() {
   return (
     <Wrapper>
       <WrapperContent>
         <InterviewRoomSection>
-          <InterviewRoomInfo />
+          <InterviewRoomInfo>
+            <TextWrapper>
+              <RoomTitle>삼성전자 데이터분석 합격자만</RoomTitle>
+              <DateInfo>2020년 10월 3일 시간 미정</DateInfo>
+              <Description>삼성전자 GSAT 합격자만 오세요. 10월 19일 면접 대비하려고 합니다.</Description>
+            </TextWrapper>
+            <BoxWrapper>
+              <Button text="방 나가기" theme="gray" />
+              <Button text="스터디 시작하기" theme="blue" />
+            </BoxWrapper>
+          </InterviewRoomInfo>
         </InterviewRoomSection>
         <ChatWithUser />
       </WrapperContent>
