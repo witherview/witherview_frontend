f0362ccb485cd9dfb8c5c0ef6355170edc64703e
dididy <dev.yongjaelee@gmail.com>
2020-12-05 21:00:43 +0900

    refactor: SelfTrainModal 에서 이동할 페이징 설정 및 코드 리팩토링
    - history.push 로직 추가
    - div로 감싸진 계층 제거 <- 사용하지 않아도 됨

--- src/components/Modal/SelfTrainStartModal/SelfTrainStartModal.js
+++ src/components/Modal/SelfTrainStartModal/SelfTrainStartModal.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { get } from '@utils/snippet';
+import { useHistory } from 'react-router-dom';
 import Button from '@components/Button';
 import Icon from '@components/Icon';
 import { hideModal } from '@store/Modal/modal';
@@ -8,52 +10,54 @@ import { MODALS } from '@utils/constant';
 import { ResetQuestions } from '@store/Question/question';
 
 const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 718px;
-    height: 470px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 718px;
+  height: 470px;
 `;
 
 const CancelWrapper = styled.div`
-    align-self: flex-end;
-    margin: 26px 26px 11px 0;
+  align-self: flex-end;
+  margin: 26px 26px 11px 0;
 `;
 
 const TextSave = styled.div`
-    margin-top: 25px;
-    font-family: AppleSDGothicNeoEB00;
-    font-size: 36px;
-    font-weight: normal;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: 1.44;
-    letter-spacing: normal;
-    text-align: left;
-    color: #000000;
+  margin-top: 25px;
+  font-family: AppleSDGothicNeoEB00;
+  font-size: 36px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.44;
+  letter-spacing: normal;
+  text-align: left;
+  color: #000000;
 `;
 
 const TextStart = styled.div`
-    margin: 20px 0 73px 0;
-    font-family: AppleSDGothicNeoM00;
-    font-size: 24px;
-    font-weight: normal;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: 1.33;
-    letter-spacing: normal;
-    text-align: left;
-    color: #3d3d3d;
+  margin: 20px 0 73px 0;
+  font-family: AppleSDGothicNeoM00;
+  font-size: 24px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.33;
+  letter-spacing: normal;
+  text-align: left;
+  color: #3d3d3d;
 `;
 
 export default function SelfTrainStartModal() {
   const dispatch = useDispatch();
+  const history = useHistory();
+  const { selectedQnaId } = useSelector(get('train'));
   const handleHideModal = () => {
     dispatch(hideModal(MODALS.SELF_TRAIN_START_MODAL));
   };
 
   const handleStart = () => {
-    // TODO: 혼자 연습하기 가기
+    history.push(`/self/setting/${selectedQnaId}`);
     dispatch(ResetQuestions());
     dispatch(hideModal(MODALS.SELF_TRAIN_START_MODAL));
   };
@@ -67,9 +71,7 @@ export default function SelfTrainStartModal() {
         <Icon type="check_large" alt="" />
         <TextSave>질문 리스트가 저장되었습니다.</TextSave>
         <TextStart>이제 면접 연습을 시작해보세요!</TextStart>
-        <div onClick={handleStart}>
-          <Button text="면접 시작하기" theme="blue" />
-        </div>
+        <Button text="면접 시작하기" theme="blue" func={handleStart} />
       </Wrapper>
     </>
   );
