f5b8f1c30fd6fbc45df7af4caa46f212dd58b573
Ki Tae Park <rlxo1651@daum.net>
2020-12-06 03:48:32 +0900

    refactor: 유저 렌더링 part 컴포넌트화 함(용재님 피드백 반영) - #102

--- src/pages/InterviewStudyEntry/InterviewStudyEntry.js
+++ src/pages/InterviewStudyEntry/InterviewStudyEntry.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
 
 import Button from '@components/Button/Button';
-import ProfileIcon from '@components/ProfileIcon/ProfileIcon';
-import ToggleButton from '@components/ToggleButton/ToggleButton';
 
 import { getInterviewStudyRoomAPI } from '@repository/interviewStudyRepository';
 import S from './InterviewStudy.style';
+import UsersSection from './UsersSection';
 
 export default function InterviewStudyEntry({ id }) {
   const DEFAULT_ID = 421;
@@ -59,76 +58,7 @@ export default function InterviewStudyEntry({ id }) {
               <Button text="스터디 시작하기" theme="blue" />
             </S.BoxWrapper>
           </S.InterviewRoomInfo>
-          <S.UserInfoWrapper>
-            <S.UserInfoHeader>
-              <S.TableColumn color="blue" value={88}>사용자 정보</S.TableColumn>
-              <S.TableColumn color="blue" value={420}>스터디 참가 횟수</S.TableColumn>
-              <S.TableColumn color="blue" value={627}>신뢰도</S.TableColumn>
-              <S.TableColumn color="blue" value={1023}>참가 희망</S.TableColumn>
-            </S.UserInfoHeader>
-            <S.UserInfoContent>
-              <S.ContentRow>
-                <S.TableColumn value={88}>
-                  <ProfileIcon isSmall />
-                  <S.UserInfo>
-                    <p>이용재</p>
-                    <S.TagsWrapper>
-                      <S.TagItem color="blue">
-                        유통
-                      </S.TagItem>
-                      <S.TagItem color="orange">
-                        데이터 분석
-                      </S.TagItem>
-                    </S.TagsWrapper>
-                  </S.UserInfo>
-                </S.TableColumn>
-                <S.TableColumn value={420}>
-                  <S.ItemInnerWrapper>
-                    10번
-                  </S.ItemInnerWrapper>
-                </S.TableColumn>
-                <S.TableColumn color="blue" value={627}>
-                  <S.ItemInnerWrapper>
-                    <S.ProgressBar max="100" value="35" />
-                    35%
-                  </S.ItemInnerWrapper>
-                </S.TableColumn>
-                <S.TableColumn color="blue" value={1023}>
-                  <ToggleButton />
-                </S.TableColumn>
-              </S.ContentRow>
-              <S.ContentRow>
-                <S.TableColumn value={88}>
-                  <ProfileIcon isSmall />
-                  <S.UserInfo>
-                    <p>김찬호</p>
-                    <S.TagsWrapper>
-                      <S.TagItem color="blue">
-                        화학
-                      </S.TagItem>
-                      <S.TagItem color="orange">
-                        데이터 분석
-                      </S.TagItem>
-                    </S.TagsWrapper>
-                  </S.UserInfo>
-                </S.TableColumn>
-                <S.TableColumn value={420}>
-                  <S.ItemInnerWrapper>
-                    12번
-                  </S.ItemInnerWrapper>
-                </S.TableColumn>
-                <S.TableColumn color="blue" value={627}>
-                  <S.ItemInnerWrapper>
-                    <S.ProgressBar max="100" value="100" />
-                    100%
-                  </S.ItemInnerWrapper>
-                </S.TableColumn>
-                <S.TableColumn color="blue" value={1023}>
-                  <ToggleButton />
-                </S.TableColumn>
-              </S.ContentRow>
-            </S.UserInfoContent>
-          </S.UserInfoWrapper>
+          <UsersSection />
         </S.InterviewRoomSection>
       </S.WrapperContent>
     </S.Wrapper>
