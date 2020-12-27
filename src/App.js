a9f91f198acccdba780ee00ae2209613a19eb433
dididy <dev.yongjaelee@gmail.com>
2020-12-27 18:21:22 +0900

    Revert "refactor(optimize): chunk 크기 줄이기 위해 React.lazy 적용 (#129) - wip"
    This reverts commit 007e779b420438cf05fdaa44c6109c3ac15b6595.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -18,18 +18,16 @@ import SelfTrainSettingPage from '@pages/SelfTrainSettingPage';
 import SelfTrainPage from '@pages/SelfTrainPage';
 
 import AloneQuestionCheckList from '@pages/AloneQuestionCheckList';
+import MyPage from '@pages/MyPage';
+import StudyMainPage from '@pages/StudyMainPage';
+import InterviewStudyEntry from '@pages/InterviewStudyEntry';
 
 import MyVideoPage from '@pages/MyVideoPage';
 import VideoPage from '@pages/VideoPage';
 
 import Sidebar from '@components/Sidebar';
 import ProfileMenuContainer from '@components/ProfileMenuContainer';
-
 import { get } from './utils/snippet';
-
-const MyPage = lazy(() => import('@pages/MyPage'));
-const StudyMainPage = lazy(() => import('@pages/StudyMainPage'));
-const InterviewStudyEntry = lazy(() => import('@pages/InterviewStudyEntry'));
 
 const Wrapper = styled.div`
   display: flex;
@@ -44,12 +42,6 @@ const WrapSpinner = styled.div`
   align-items: center;
 `;
 
-const loading = () => (
-  <WrapSpinner>
-    <SyncLoader size={50} color="#123abc" />
-  </WrapSpinner>
-);
-
 export default function App() {
   const { name } = useSelector(get('auth'));
   const { toggleTrain, isLoading } = useSelector(get('train'));
@@ -60,37 +52,37 @@ export default function App() {
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/login" component={LoginPage} />
         {isLoading && (
-          loading
+          <WrapSpinner>
+            <SyncLoader size={50} color="#123abc" />
+          </WrapSpinner>
         )}
         <Wrapper>
           <>
             {!toggleTrain && <Sidebar />}
             {!toggleTrain && <ProfileMenuContainer name={name} />}
-            <Suspense fallback={loading}>
+            <AuthRoute
+              exact
+              path="/questionlist"
+              component={QuestionListPage}
+            />
+            <AuthRoute path="/question/:id" component={QuestionPage} />
+            <AuthRoute exact path="/self" component={SelfTrainEntryPage} />
+            <AuthRoute
+              path="/self/setting/:id"
+              component={SelfTrainSettingPage}
+            />
+            <AuthRoute path="/self-train/:id" component={SelfTrainPage} />
+            <AuthRoute
+              exact
+              path="/self-checklist/:id"
+              component={AloneQuestionCheckList}
+            />
+            <AuthRoute path="/group-study" component={StudyMainPage} />
+            <AuthRoute path="/study-room/:id" component={InterviewStudyEntry} />
+            <AuthRoute exact path="/mypage" component={MyPage} />
 
-              <AuthRoute
-                exact
-                path="/questionlist"
-                component={QuestionListPage}
-              />
-              <AuthRoute path="/question/:id" component={QuestionPage} />
-              <AuthRoute exact path="/self" component={SelfTrainEntryPage} />
-              <AuthRoute
-                path="/self/setting/:id"
-                component={SelfTrainSettingPage}
-              />
-              <AuthRoute path="/self-train/:id" component={SelfTrainPage} />
-              <AuthRoute
-                exact
-                path="/self-checklist/:id"
-                component={AloneQuestionCheckList}
-              />
-              <AuthRoute path="/group-study" component={StudyMainPage} />
-              <AuthRoute path="/study-room/:id" component={InterviewStudyEntry} />
-              <AuthRoute exact path="/mypage" component={MyPage} />
-              <Route exact path="/myvideo" component={MyVideoPage} />
-              <Route exact path="/video/:id" component={VideoPage} />
-            </Suspense>
+            <Route exact path="/myvideo" component={MyVideoPage} />
+            <Route exact path="/video/:id" component={VideoPage} />
           </>
         </Wrapper>
         <Route component={NotFound} />
