38d109d172c640c438386d8abf0280ab0414f96a
dididy <dev.yongjaelee@gmail.com>
2021-01-04 00:00:28 +0900

    feat: 회원가입 후 Welcom 페이지 퍼블리싱 완성
    - 은비님 디자인에 맞춰서 작업

---
+++ src/pages/WelcomePage/WelcomePage.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import WelcomePage from './WelcomePage';
+
+export default {
+  title: 'pages/Main/Welcome',
+  component: WelcomePage,
+};
+
+export const Welcome = () => <WelcomePage />;
