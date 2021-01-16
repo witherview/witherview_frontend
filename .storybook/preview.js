5291593c25f20fe6715cea9d8b95aed0e27ac829
dididy <dev.yongjaelee@gmail.com>
2021-01-16 16:34:34 +0900

    refactor(storybook): 필요없는 storybook 옵션들 삭제
    - redux 관련한 옵션들인데 해당 옵션들이 없어도 잘 동작하므로 삭제

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { addDecorator } from "@storybook/react";
-import GlobalStyles from "../src/style/globalStyles";
-import { Provider } from "react-redux";
-import { ThemeProvider } from "styled-components";
-import theme from "../src/style/theme";
-import store from "@store";
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import GlobalStyles from '../src/style/globalStyles';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import theme from '../src/style/theme';
+import store from '@store';
 
 addDecorator((Story) => (
   <Provider store={store}>
