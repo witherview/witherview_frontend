8697efc1ff92e1242268215b087967288260f779
YongjaeLee <dev.yongjaelee@gmail.com>
2021-02-23 15:07:02 +0900

    refactor: 💡 node socket 시그널링 서버 동작 정상화 완료
    - 아직 disconnect 부분 좀 더 정상 작동하도록 해야 함

--- __mock-server/server.js
+++ __mock-server/server.js
@@ -7,7 +7,9 @@ const app = express();
 const server = http.createServer(app);
 const socket = require('socket.io');
 
-const io = socket(server);
+const io = socket(server, {
+  path: '/socket',
+});
 
 const users = {};
 
@@ -16,7 +18,7 @@ const socketToRoom = new Map();
 io.on('connection', (sock) => {
   sock.on('join room', (roomID) => {
     if (users[roomID]) {
-      const { length } = users[roomID].size;
+      const { length } = users[roomID].length;
       if (length === 2) {
         sock.emit('room full');
         return;
@@ -59,17 +61,17 @@ io.on('connection', (sock) => {
   // todo: 브라우저 종료할 경우의 종료 로직.
   // 커넥션을 종료할 때
   sock.on('disconnect', () => {
-    let roomId = socketToRoom.get(sock.id);
-    if (roomId === undefined) { 
+    const roomId = socketToRoom.get(sock.id);
+    if (roomId === undefined) {
       // todo: 해당 방에 존재하지 않는 커넥션이 들어올 때. 예외처리가 필요할까?
       return;
-    } 
+    }
     // 해당 방에 특정 유저가 떠났다는 user left 메시지를 보낸다.
-    sock.broadcast.to(roomId).emit("user left", sock.id);
+    sock.broadcast.emit('user left', sock.id);
 
     // 서버에 저장된 데이터 삭제.
     socketToRoom.delete(sock.id);
-    users[roomID].filter((id) => id !== sock.id);
+    users[roomId].filter((id) => id !== sock.id);
   });
 });
 
