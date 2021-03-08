eddc4e9eb6315414bcf3e07ea3c0c049e725cf13
jinsunee <jsunee.dev@gmail.com>
2021-03-08 15:56:32 +0900

    feat: 🎸 calendar 초기setup

---
+++ src/components/organisms/Calendar/Calendar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Calendar() {
+  return <Container>123</Container>;
+}
+
+const Container = styled.div`
+`;
+
+export default Calendar;
---
+++ src/components/organisms/Calendar/Calendar.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import _Calendar from './Calendar';
+
+const Calendar = _Calendar;
+
+export default {
+  title: 'Organisms/Calendar',
+  component: Calendar,
+};
+
+export const interviewChat = (args) => <Calendar {...args} />;
