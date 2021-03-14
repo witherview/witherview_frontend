1a5e3294c28ecae78aa87e421d58ac6e8c95d654
YongjaeLee <dev.yongjaelee@gmail.com>
2021-03-15 00:36:41 +0900

    feat: 🎸 만들지 않고 누락됐던 SignUpPage 스토리 생성한다 (#38)
    - 스토리를 미처 못만들었었음

---
+++ src/pages/SignUpPage/SignUpPage.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import SignUpPage from './SignUpPage';
+
+export default {
+  title: 'pages/Sign Up',
+  component: SignUpPage,
+};
+
+export const signUp = () => <SignUpPage />;
