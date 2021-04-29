8782de184df3c3d1ded1b23b40c3ebe9e31d2eb3
YongjaeLee <dev.yongjaelee@gmail.com>
2021-04-29 23:33:06 +0900

    style: 💄 EvaluationModal merge conflict 해결 실수한 부분을 반영한다
    - hideModal을 removeModal로 변경

--- src/components/organisms/Modal/EvaluationModal/EvaluationModal.js
+++ src/components/organisms/Modal/EvaluationModal/EvaluationModal.js
@@ -172,7 +172,7 @@ export default function EvaluationModal({ roomId }) {
     try {
       await postGroupFeedback(data);
 
-      dispatch(hideModal(MODALS.EVALUATION_MODAL));
+      dispatch(removeModal(MODALS.EVALUATION_MODAL));
 
       history.push('/peer-study');
     } catch (error) {
@@ -189,7 +189,9 @@ export default function EvaluationModal({ roomId }) {
         <A.Icon
           type="cancel_blue"
           alt=""
-          func={() => dispatch(removeModal({ modalName: MODALS.EVALUATION_MODAL }))}
+          func={() =>
+            dispatch(removeModal({ modalName: MODALS.EVALUATION_MODAL }))
+          }
         />
       </IconWrapper>
       <ResultText>면접 최종 평가</ResultText>
