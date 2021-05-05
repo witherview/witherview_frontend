b3c9b6e6e23df0e2cd2be6858f2cbd1557ca23f7
wooooooood <13pft13@gmail.com>
2021-05-05 23:21:45 +0900

    feat: 🎸 x 아이콘 컴포넌트를 추가한다

---
+++ src/components/atoms/CloseIcon/CloseIcon.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const Close = styled.div`
+  &:after {
+    content: '\\00d7';
+    color: ${({ color }) => color};
+    font-size: 17px;
+  }
+`;
+
+export default function CloseIcon({ func, color }) {
+  return <Close onClick={func} style={{ color }} />;
+}
+
+CloseIcon.propTypes = {
+  func: PropTypes.func,
+  color: PropTypes.string,
+};
+
+CloseIcon.defaultProps = {
+  func: () => {},
+  color: 'black',
+};
---
+++ src/components/atoms/CloseIcon/CloseIcon.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import CloseIcon from './CloseIcon';
+
+export default {
+  title: 'Atoms/Close Icon',
+  component: CloseIcon,
+};
+
+export const closeIcon = (args) => <CloseIcon {...args} />;
