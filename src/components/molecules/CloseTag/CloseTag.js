a064e667eca2a80d1004a047f2cb9766d1f66015
wooooooood <13pft13@gmail.com>
2021-05-09 18:14:16 +0900

    feat: 🎸 관심 산업/직무 X컴포넌트를 추가한다
    ✅ Closes: #43

---
+++ src/components/molecules/CloseTag/CloseTag.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import CloseIcon from '../../atoms/CloseIcon';
+import theme from '../../../style/theme';
+
+const Wrapper = styled.div`
+  display: inline-flex;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  border: solid 0.2vh ${({ backgroundColor }) => backgroundColor};
+  border-radius: 20px;
+  padding: 0.9vh 1.8vh;
+`;
+const Content = styled.p`
+  color: ${({ contentColor }) => contentColor};
+  display: inline-block;
+  margin-right: 1.4vh;
+  font-size: 20px;
+`;
+
+export default function Tag({ func, backgroundColor, text, contentColor }) {
+  return (
+    <Wrapper onClick={func} backgroundColor={backgroundColor}>
+      <Content contentColor={contentColor}>{text}</Content>
+      <CloseIcon color={contentColor} />
+    </Wrapper>
+  );
+}
+
+Tag.propTypes = {
+  func: PropTypes.func,
+  backgroundColor: PropTypes.string,
+  text: PropTypes.string,
+  contentColor: PropTypes.string,
+};
+
+Tag.defaultProps = {
+  func: () => {},
+  backgroundColor: theme.colors.cornflower,
+  text: '화학',
+  contentColor: 'white',
+};
---
+++ src/components/molecules/CloseTag/CloseTag.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import CloseTag from './CloseTag';
+
+export default {
+  title: 'Molecules/Close Tag',
+  component: CloseTag,
+};
+
+export const closeTag = (args) => <CloseTag {...args} />;
