ad28a17e334d97270603a9443157aea7ccff3953
wooooooood <13pft13@gmail.com>
2021-05-09 18:30:26 +0900

    feat: 🎸 관심 산업/직무 +컴포넌트를 추가한다

---
+++ src/components/molecules/AddTag/AddTag.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import AddTag from './AddTag';
+
+export default {
+  title: 'Molecules/Add Tag',
+  component: AddTag,
+};
+
+export const addTag = (args) => <AddTag {...args} />;
