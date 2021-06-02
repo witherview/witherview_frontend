e99bb50f561454efe18e990502e2fe670287e7e0
YONGJAE LEE <dev.yongjaelee@gmail.com>
2021-06-02 09:25:55 +0900

    refactor: 💡 SeltTrainChecklistPage의 스타일을 파일로 분리한다
    * 하나의 파일이 너무 길어져서 스타일은 분리

--- src/pages/SelfTrainChecklistPage/SelfTrainChecklistPage.js
+++ src/pages/SelfTrainChecklistPage/SelfTrainChecklistPage.js
@@ -5,7 +5,6 @@
 import React, { useState, useEffect, useCallback, useRef } from 'react';
 
 import axios from 'axios';
-import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
@@ -23,246 +22,7 @@ import {
 import { get } from '@utils/snippet';
 import { evaluation, flatEvaluation } from '@mocks/EvaluationListMock';
 import A from '@atoms';
-
-const CloseButton = styled.div`
-  position: absolute;
-  top: 5.3vh;
-  right: 10.5vh;
-  border: none;
-  cursor: pointer;
-  background-color: transparent;
-  &:focus {
-    outline: none;
-  }
-`;
-
-const Wrapper = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-color: #eef0ff;
-`;
-
-const WrapContent = styled.div`
-  height: 80vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const EndingTitle = styled.span`
-  display: block;
-  color: #000000;
-  font-family: AppleSDGothicNeoEB00;
-  font-size: 3.1vh;
-  padding-top: 3.9vh;
-`;
-
-const Content = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  margin-top: 9vh;
-`;
-
-const LeftContent = styled.div`
-  height: 67.1vh;
-  width: 47vh;
-  padding-top: 3vh;
-`;
-
-const RightContent = styled.div`
-  height: 67.1vh;
-  margin-left: 6.1vh;
-`;
-
-const VideoContainer = styled.figure`
-  width: 47vh;
-  margin: 0;
-
-  & video {
-    width: 100%;
-    border-radius: 2vh;
-    box-shadow: 0 0.6vh 2.4vh 0 rgba(4, 4, 161, 0.04);
-  }
-`;
-
-const ControlWrapper = styled.div`
-  overflow: hidden;
-  background-color: #ffffff;
-  width: 100%;
-  position: relative;
-  border-radius: 1vh;
-  margin-top: 1vh;
-  padding: 1vh;
-  box-sizing: border-box;
-  box-shadow: 0 0.6vh 1.2vh 0 rgba(4, 4, 161, 0.1);
-`;
-
-const ButtonWrapper = styled.button`
-  height: 5.1vh;
-  width: 5.1vh;
-  > button {
-    height: 5.1vh;
-    width: 5.1vh;
-  }
-  display: block;
-  height: 100%;
-  float: left;
-  border: none;
-  cursor: pointer;
-  background: transparent;
-  margin: 0;
-  padding: 0;
-
-  &:focus {
-    outline: none;
-  }
-  &:hover {
-    opacity: 0.5;
-  }
-`;
-
-const ProgressWrapper = styled.div`
-  display: block;
-  width: 80%;
-  height: 100%;
-  float: left;
-  margin-left: 5%;
-  padding: 0;
-
-  & progress {
-    display: block;
-    height: 2vh;
-    width: 100%;
-    margin-top: 2vh;
-    border: none;
-    overflow: hidden;
-    -moz-border-radius: 0.2vh;
-    -webkit-border-radius: 0.2vh;
-    border-radius: 0.2vh;
-    color: #6e6eff;
-    cursor: pointer;
-    &::-moz-progress-bar {
-      background-color: #6e6eff;
-    }
-    &::-webkit-progress-value {
-      background-color: #6e6eff;
-    }
-    &::-webkit-progress-bar {
-      background-color: #d3d3d3;
-    }
-  }
-`;
-
-const CheckPointWrapper = styled.div`
-  position: relative;
-  background: transparent;
-
-  & > div {
-    position: absolute;
-    cursor: pointer;
-  }
-`;
-
-const ButtonsWrapper = styled.div`
-  height: 18vh;
-  width: inherit;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  margin-top: 5.5vh;
-
-  > div {
-    width: 22.5vh;
-    height: 6vh;
-    > p {
-      font-size: 1.9vh;
-    }
-    box-shadow: 0 0.6vh 2.4vh 0 rgba(4, 4, 161, 0.04);
-  }
-`;
-
-const CheckListContainer = styled.div`
-  display: inline-block;
-  width: 52vh;
-  height: 67vh;
-  box-sizing: border-box;
-  margin-right: 2.6vh;
-  padding: 4.3vh 5.5vh 0 5.5vh;
-  border-radius: 1vh;
-  box-shadow: 0 0.6vh 1.2vh 0 rgba(4, 4, 161, 0.1);
-  background-color: #ffffff;
-  font-family: AppleSDGothicNeoM00;
-  text-align: left;
-
-  & > ul {
-    margin: 0;
-    padding: 0;
-    list-style: none;
-
-    li {
-      display: table;
-      font-size: 1.5vh;
-      font-weight: normal;
-      font-stretch: normal;
-      font-style: normal;
-      text-align: left;
-      letter-spacing: normal;
-      text-align: left;
-      color: #3d3d3d;
-      margin-bottom: 2vh;
-
-      i {
-        margin-right: 2.5vh;
-        cursor: pointer;
-        vertical-align: middle;
-      }
-
-      span {
-        display: table-cell;
-        vertical-align: middle;
-        line-height: 1.5;
-      }
-    }
-  }
-`;
-
-const CheckListTitle = styled.h2`
-  font-size: 2.5vh;
-  margin-bottom: 3vh;
-`;
-
-const CheckPoint = styled.div`
-  left: calc(${({ point }) => `${point} * 97%`});
-`;
-
-const SmallCheckList = styled.div`
-  display: inline-block;
-  vertical-align: top;
-
-  & > * {
-    display: block;
-    height: 32vh;
-    margin: 0;
-    margin-bottom: 3vh;
-  }
-`;
-
-const ChecklistEach = styled.li`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-
-  > span {
-    font-size: 1.9vh;
-  }
-`;
+import S from './SelfTrainChecklistPage.style';
 
 export default function SelfTrainChecklistPage({ match }) {
   const { roomId } = match.params;
@@ -418,22 +178,22 @@ export default function SelfTrainChecklistPage({ match }) {
   }, [localBlob]);
 
   return (
-    <Wrapper>
-      <WrapContent>
-        <CloseButton type="button">
+    <S.Wrapper>
+      <S.WrapContent>
+        <S.CloseButton type="button">
           <A.Icon
             type="cancel_circle"
             isCircle
             alt="close"
             func={() => history.push('/self')}
           />
-        </CloseButton>
-        <EndingTitle>
+        </S.CloseButton>
+        <S.EndingTitle>
           면접이 종료 되었습니다. 체크리스트를 통해 스스로 평가를 해보세요.
-        </EndingTitle>
-        <Content>
-          <LeftContent>
-            <VideoContainer>
+        </S.EndingTitle>
+        <S.Content>
+          <S.LeftContent>
+            <S.VideoContainer>
               <video src={localBlob} ref={video} controls preload="metadata">
                 <track
                   src=""
@@ -444,15 +204,15 @@ export default function SelfTrainChecklistPage({ match }) {
                 {/* TODO: 지금은 로컬 blob을 보여주고 있으나 이부분 서버 HLS url을 적용하는 방식으로 처리해야 함 */}
                 <source type="video/mp4" />
               </video>
-              <ControlWrapper>
-                <ButtonWrapper type="button" onClick={handlePlayIconClick}>
+              <S.ControlWrapper>
+                <S.ButtonWrapper type="button" onClick={handlePlayIconClick}>
                   <A.Icon
                     type={nowPlaying ? 'pause' : 'play_blue'}
                     isCircle
                     alt="play/button"
                   />
-                </ButtonWrapper>
-                <ProgressWrapper>
+                </S.ButtonWrapper>
+                <S.ProgressWrapper>
                   <progress
                     ref={progress}
                     onChange={(e) =>
@@ -464,9 +224,9 @@ export default function SelfTrainChecklistPage({ match }) {
                     step="1"
                     onClick={handleClickProgress}
                   />
-                  <CheckPointWrapper>
+                  <S.CheckPointWrapper>
                     {timeFlag.slice(0, timeFlag.length - 1)?.map((item) => (
-                      <CheckPoint
+                      <S.CheckPoint
                         point={item / video.current?.duration}
                         key={`${item}point`}
                       >
@@ -475,13 +235,13 @@ export default function SelfTrainChecklistPage({ match }) {
                           alt="section"
                           func={handleClickProgress}
                         />
-                      </CheckPoint>
+                      </S.CheckPoint>
                     ))}
-                  </CheckPointWrapper>
-                </ProgressWrapper>
-              </ControlWrapper>
-            </VideoContainer>
-            <ButtonsWrapper>
+                  </S.CheckPointWrapper>
+                </S.ProgressWrapper>
+              </S.ControlWrapper>
+            </S.VideoContainer>
+            <S.ButtonsWrapper>
               <A.Button
                 text="다시 연습하기"
                 theme="blue"
@@ -503,59 +263,59 @@ export default function SelfTrainChecklistPage({ match }) {
                 theme="white"
                 func={postChecklist}
               />
-            </ButtonsWrapper>
-          </LeftContent>
-          <RightContent>
-            <CheckListContainer>
-              <CheckListTitle>답변내용 및 목소리 체크!</CheckListTitle>
+            </S.ButtonsWrapper>
+          </S.LeftContent>
+          <S.RightContent>
+            <S.CheckListContainer>
+              <S.CheckListTitle>답변내용 및 목소리 체크!</S.CheckListTitle>
               <ul>
                 {evaluation.evaluationList1.map(({ id, text }) => (
-                  <ChecklistEach id={id} key={id}>
+                  <S.ChecklistEach id={id} key={id}>
                     <A.Icon
                       type={checkListArray[id] ? 'check_on' : 'check_off'}
                       alt="checkbox"
                       func={handleCheck}
                     />
                     <span>{text}</span>
-                  </ChecklistEach>
+                  </S.ChecklistEach>
                 ))}
               </ul>
-            </CheckListContainer>
-            <SmallCheckList>
-              <CheckListContainer>
-                <CheckListTitle>비언어 내용 체크!</CheckListTitle>
+            </S.CheckListContainer>
+            <S.SmallCheckList>
+              <S.CheckListContainer>
+                <S.CheckListTitle>비언어 내용 체크!</S.CheckListTitle>
                 <ul>
                   {evaluation.evaluationList2.map(({ id, text }) => (
-                    <ChecklistEach id={id} key={id}>
+                    <S.ChecklistEach id={id} key={id}>
                       <A.Icon
                         type={checkListArray[id] ? 'check_on' : 'check_off'}
                         alt="checkbox"
                         func={handleCheck}
                       />
                       <span>{text}</span>
-                    </ChecklistEach>
+                    </S.ChecklistEach>
                   ))}
                 </ul>
-              </CheckListContainer>
-              <CheckListContainer>
-                <CheckListTitle>영상 환경 체크!</CheckListTitle>
+              </S.CheckListContainer>
+              <S.CheckListContainer>
+                <S.CheckListTitle>영상 환경 체크!</S.CheckListTitle>
                 <ul>
                   {evaluation.evaluationList3.map(({ id, text }) => (
-                    <ChecklistEach id={id} key={id}>
+                    <S.ChecklistEach id={id} key={id}>
                       <A.Icon
                         type={checkListArray[id] ? 'check_on' : 'check_off'}
                         alt="checkbox"
                         func={handleCheck}
                       />
                       <span>{text}</span>
-                    </ChecklistEach>
+                    </S.ChecklistEach>
                   ))}
                 </ul>
-              </CheckListContainer>
-            </SmallCheckList>
-          </RightContent>
-        </Content>
-      </WrapContent>
-    </Wrapper>
+              </S.CheckListContainer>
+            </S.SmallCheckList>
+          </S.RightContent>
+        </S.Content>
+      </S.WrapContent>
+    </S.Wrapper>
   );
 }
