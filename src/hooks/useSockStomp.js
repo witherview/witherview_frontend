7e26d7b2f03a0e31be086f2789f89ad971a9445a
YONGJAE LEE <dev.yongjaelee@gmail.com>
2021-06-17 00:53:32 +0900

    fix: 🐛 stomp 연결시 token을 헤더에 포함하도록 변경한다
    * stomp 연결시 비동기 처리를 하도록 함
    * 토픽명을 변경함

--- src/hooks/useSockStomp.js
+++ src/hooks/useSockStomp.js
@@ -3,8 +3,12 @@ import SockJS from 'sockjs-client';
 import Stomp from 'stompjs';
 import moment from 'moment';
 
+const HEADER = {
+  Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`,
+};
+
 export default function useSockStomp({
-  url = 'https://api.witherview.com/socket',
+  url = 'http://api.witherview.com/socket',
   roomId,
 }) {
   const [chat, setChat] = useState([]);
@@ -12,40 +16,47 @@ export default function useSockStomp({
   const client = useRef(null);
 
   const handleClick = (payload) => {
-    console.log(payload);
-
     const newMessage = {
       type: 'COMMENT',
       roomId,
       sender: sessionStorage.getItem('name'),
       contents: payload,
     };
-    client.current.send('/pub/chat', {}, JSON.stringify(newMessage));
+    client.current.send('/pub/chat.room', HEADER, JSON.stringify(newMessage));
   };
 
   useEffect(() => {
-    client.current = Stomp.over(new SockJS(url));
-    client.current.debug = null;
-    client.current.connect(
-      {},
-      () => {
-        setIsConnectStomp(true);
-        client.current.subscribe(`/sub/room/${roomId}`, (data) => {
-          const newMessage = JSON.parse(data.body);
-          console.log(newMessage);
+    (async () => {
+      client.current = await Stomp.over(new SockJS(url));
 
-          const chatData = {
-            time: moment(new Date()).format('HH:mm A'),
-            name: newMessage.sender,
-            content: newMessage.contents,
-          };
-          setChat((prev) => [...prev, chatData]);
-        });
-      },
-      (err) => {
-        console.error(err);
-      },
-    );
+      // client.current.debug = null;
+
+      await client.current.connect(
+        HEADER,
+        () => {
+          setIsConnectStomp(true);
+          client.current.subscribe(
+            `/sub/room.${roomId}`,
+            (data) => {
+              const newMessage = JSON.parse(data.body);
+              console.log(newMessage);
+
+              const chatData = {
+                time: moment(new Date()).format('HH:mm A'),
+                name: newMessage.sender,
+                content: newMessage.contents,
+              };
+              setChat((prev) => [...prev, chatData]);
+            },
+            HEADER,
+          );
+        },
+        (err) => {
+          console.error(err);
+        },
+      );
+    })();
+
     return () => {
       client.current.disconnect();
     };
