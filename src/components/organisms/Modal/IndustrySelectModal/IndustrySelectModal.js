a583428e5b5eeb9782192c7deaf2cdfc9c35c927
wooooooood <13pft13@gmail.com>
2021-06-20 15:23:26 +0900

    fix: 🐛 산업태그 선택 모달에서 필요없는 eslint 설정을 비활성화한다
    ✅ Closes: #44

--- src/components/organisms/Modal/IndustrySelectModal/IndustrySelectModal.js
+++ src/components/organisms/Modal/IndustrySelectModal/IndustrySelectModal.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
