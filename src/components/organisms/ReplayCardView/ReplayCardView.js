26e3ee9f6ff33aac1b21c510501e31d2e91ac6dc
YONGJAE LEE <dev.yongjaelee@gmail.com>
2021-07-03 23:34:15 +0900

    fix: 🐛 ReplayCardView 의 퍼블리싱을 정상화한다
    * 기존의 디자인 되어있는 부분 수정

--- src/components/organisms/ReplayCardView/ReplayCardView.js
+++ src/components/organisms/ReplayCardView/ReplayCardView.js
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import tempImg from '@assets/images/box_c_three.png';
 import A from '@atoms';
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   border-radius: 1vh;
-  width: 28vh;
+  width: 35.3vh;
   height: 36.8vh;
   box-shadow: 0 0.6vh 1.2vh 0 rgba(4, 4, 161, 0.1);
   box-sizing: border-box;
@@ -15,14 +14,19 @@ const Wrapper = styled.div`
 
 const SnapshotArea = styled.div`
   width: 100%;
-  height: 50%;
+  height: 18.9vh;
   border-radius: 1vh 1vh 0 0;
   position: relative;
   box-sizing: border-box;
 
-  img {
+  .thumbnail {
     width: 100%;
     height: 100%;
+
+    background-color: black;
+
+    border-top-left-radius: 1vh;
+    border-top-right-radius: 1vh;
   }
 
   i {
@@ -37,18 +41,24 @@ const SnapshotArea = styled.div`
 `;
 
 const DescriptionArea = styled.div`
-  height: 50%;
-  padding: 1vh 4.3vh;
+  height: 17.9vh;
+  padding: 3.7vh 4.3vh;
+
+  box-sizing: border-box;
+
   line-height: 1.5;
 
-  div:first-child {
-    display: flex;
-    flex-direction: row-reverse;
-    box-sizing: border-box;
+  position: relative;
+
+  i:first-child {
+    position: absolute;
+
+    right: 2.5vh;
+    top: 1.7vh;
   }
 
   .recDate {
-    font-size: 2vh;
+    font-size: 1.6vh;
     color: #d3d3d3;
   }
 
@@ -56,38 +66,73 @@ const DescriptionArea = styled.div`
     display: flex;
     padding-top: 1.6vh;
     padding-bottom: 1vh;
-    font-size: 2.6vh;
+    font-size: 2.2vh;
+    font-family: AppleSDGothicNeoB00;
   }
 
   .videoTime {
-    font-size: 2.2vh;
-    color: #9e9e9e;
+    font-size: 1.6vh;
+    color: #d3d3d3;
+
+    .bold {
+      color: #9e9e9e;
+    }
   }
 `;
 
+const time = 1000;
 export default function ReplayCardView() {
   const [recDate] = useState('12월 1일 PM 20:00');
-  const [videoTime] = useState('20min 15 sec 혼자연습');
   const [title] = useState('12월 1일 혼자연습');
+
+  const [pt, setPt] = useState({ hour: '', min: '', sec: '' });
+
+  useEffect(() => {
+    setPt({
+      hour: Math.floor(time / 3600) > 0 ? Math.floor(time / 3600) : '',
+      min:
+        Math.floor((time % 3600) / 60) > 0
+          ? Math.floor((time % 3600) / 60)
+          : '',
+      sec: (time % 3600) % 60,
+    });
+  }, []);
 
   return (
     <Wrapper className="replayCardItem">
       <SnapshotArea>
-        <img src={tempImg} alt="temp" />
+        <div className="thumbnail" src="" alt="temp" />
         <A.Icon type="play_pupple" />
       </SnapshotArea>
 
       <DescriptionArea>
-        <div>
-          <A.Icon type="clock_black" alt="" />
-        </div>
+        <A.Icon className="abc" type="dots" alt="" />
+
         <p className="recDate">{recDate}</p>
         <div className="titleArea">
           <p>{title}</p>
-          <A.Icon type="clock_black" alt="" />
         </div>
 
-        <p className="videoTime">{videoTime}</p>
+        <p className="videoTime">
+          {pt.hour !== '' && (
+            <>
+              <span className="bold">{pt.hour} </span>
+              <span>hour</span>
+            </>
+          )}
+          {pt.min !== '' && (
+            <>
+              <span className="bold"> {pt.min} </span>
+              <span>min</span>
+            </>
+          )}
+          {pt.sec !== '' && (
+            <>
+              <span className="bold"> {pt.sec} </span>
+              <span>sec</span>
+            </>
+          )}
+        </p>
       </DescriptionArea>
     </Wrapper>
   );
