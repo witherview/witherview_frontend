4d6501dfcbc81b6f9eb1185a33a3ac66d8c3a6ab
Sung-jin <osj4872@gmail.com>
2021-07-11 23:29:19 +0900

    feat: 🎸 storybook 추가
    ✅ Closes: #118

--- src/pages/PasswordPage/PasswordPage.stories.js
+++ src/pages/PasswordPage/PasswordPage.stories.js
@@ -1,10 +1,10 @@
-// import React from 'react';
-//
-// import LoginPage from './LoginPage';
-//
-// export default {
-//   title: 'pages/Main/Login',
-//   component: LoginPage,
-// };
-//
-// export const login = () => <LoginPage />;
+import React from 'react';
+
+import PasswordPage from './PasswordPage';
+
+export default {
+  title: 'pages/Main/Password',
+  component: PasswordPage,
+};
+
+export const login = () => <PasswordPage />;
